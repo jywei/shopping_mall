c4e3eb3487964e101ffe54e3b18508931389d402
Roy Wei <roywjy@gmail.com>
2017-02-01 22:57:16 +0800

    quantity function

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -3,9 +3,20 @@ require 'rails_helper'
 RSpec.describe Cart, type: :model do
   describe "Basic functions of a shopping cart" do
     it "is able to put an item into a shopping cart" do
+      cart = Cart.new
+      cart.add_item             1
+      expect(cart.empty?).to be false
     end
 
     it "if the same item is added, CartItem won't increase, but the quantity will" do
+      cart = Cart.new
+      3.times { cart.add_item(1) }
+      5.times { cart.add_item(2) }
+      2.times { cart.add_item(3) }
+
+      expect(cart.items.length).to be          3
+      expect(cart.items.first.quantity).to be  3
+      expect(cart.items.second.quantity).to be 5
     end
 
     it "an item is able to be added or cancelled in the shopping cart" do
